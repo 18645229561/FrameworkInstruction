41f78f7a543c09e388b2b7b0dbe4ff6caa0cd214
renpeng <18645229561@163.com>
2016-11-13 15:09:44 +0800

    mark

--- app/src/main/java/com/example/renpeng/frameworkinstruction/MainActivity.java
+++ app/src/main/java/com/example/renpeng/frameworkinstruction/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.renpeng.frameworkinstruction;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Activity实现了Window.CallBack接口,并且在attach方法中将自身作为Window对象传递给CallBack接口实现
+
+    /**
+     * WindowManagerImp类维护着三个数组
+     * View[] 每个view对象
+     * ViewRoot[] 每个View对象对应的ViewRoot
+     * WindowManagerImp.LayoutParams[]
+     * */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +21,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * 该方法是Window添加完成setContent的布局后调用的,通知Activity
+     * 该方法是Window添加完成setContent的布局后的回调,通知Activity
      * */
     @Override
     public void onContentChanged() {
         super.onContentChanged();
     }
+
 }
